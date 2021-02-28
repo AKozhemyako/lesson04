21a171de89cbcfc2215ca301a425e60ef5bcf6f0
AKozhemyako <admin_tp@ukr.net>
2021-02-28 13:49:09 +0200

    read file by lines

--- string/string_game.py
+++ string/string_game.py
@@ -23,11 +23,19 @@ print(text_file.readline())
 text_file.close()
 print('Открываю файл ssh.txt.')
 text_file = open('ssh.txt', 'r', encoding='utf-8')
+print('Читаю строки с 1 открыторго файла')
 lines = text_file.readlines()
 print(lines)
 print(len(lines))
 for line in lines:
     print(line)
+print('и закрываю файл...')
 text_file.close()
+print('Перебор файла по строчно.')
+text_file = open('ssh.txt', 'r', encoding='utf-8')
+for line in text_file:
+    print(line)
+text_file.close()
+input('\n\nНажмите Enter, чтобы выйти.')
 
 
